33a2a48f61fd39a6324255970ceccb46ac7c2502
brunerMatthew <bruner.matthewm@gmail.com>
2024-05-10 17:12:29 -0600

    first commit

---
+++ functions.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+)
+
+type Data struct {
+	Main Main `json:"main"`
+	Wind Wind `json:"wind"`
+}
+
+type Main struct {
+	Temp     float64 `json:"temp"`
+	Humidity float64 `json:"humidity"`
+}
+
+type Wind struct {
+	Speed float64 `json:"speed"`
+}
+
+func (m *Main) kelvinToCelsius() {
+	m.Temp = m.Temp - 273.15
+}
+
+func fetchLiveWeatherData() {
+	url := "https://api.openweathermap.org/data/2.5/weather?lat=40.65&lon=-111.85&appid=25a3874cd58d5c0d253a5a7fc33f9ebe"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data Data
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data.Main.kelvinToCelsius()
+
+	dataChannel <- data
+}
+
+func predict(w http.ResponseWriter, r *http.Request) {
+
+	data := <-dataChannel
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := bigquery.NewClient(ctx, "slc-air-quality")
+	if err != nil {
+		log.Printf("Failed to create BigQuery client: %v", err)
+		http.Error(w, "Failed to create BigQuery client", http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
+
+	predictionQuery := fmt.Sprintf(
+		`
+		SELECT *
+		FROM ML.PREDICT(MODEL %s,
+		  (
+		  SELECT
+		    TIMESTAMP("%v") AS date_timestamp,
+		    NULL AS precipitation,
+		    %v AS wind,
+		    %v AS temperature,
+		    %v AS humidity,
+		    NULL AS dew_point
+		  )
+		)
+		`,
+		"`slc-air-quality.meteorology.aqi_model`",
+		time.Now().Format("2006-01-02"),
+		data.Wind.Speed,
+		data.Main.Temp,
+		data.Main.Humidity)
+
+	query := client.Query(predictionQuery)
+	it, err := query.Read(ctx)
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		http.Error(w, "Failed to execute query", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	// w.Write([]byte("Row inserted successfully"))
+	fmt.Fprintln(w, it)
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+var (
+	dataChannel chan Data = make(chan Data, 1)
+)
+
+func main() {
+	http.HandleFunc("GET /predict", predictHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func predictHandler(w http.ResponseWriter, r *http.Request) {
+	go fetchLiveWeatherData()
+	predict(w, r)
+}
